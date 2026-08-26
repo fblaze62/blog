4ae47d6ace10bffb41f6074b37fed33ee36b1620
fblaze62 <feablaze@outlook.com>
2026-08-26 11:36:21 +0800

    style: 添加了一些壁纸背景

--- src/config/displaySettingsConfig.ts
+++ src/config/displaySettingsConfig.ts
@@ -52,7 +52,7 @@ export const displaySettingsConfig: DisplaySettingsConfig =
 		bannerTitleSwitchable: false,
 
 		// 壁纸轮播开关
-		bannerCarouselSwitchable: false,
+		bannerCarouselSwitchable: true,
 
 		// 全屏透明模式参数调节开关
 		// 设为 false 关闭所有滑块，或用对象形式单独控制每个滑块
