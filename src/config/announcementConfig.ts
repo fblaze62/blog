558712fd6ba8bdf964405d53992d00b7c0425c05
fblaze62 <feablaze@outlook.com>
2026-09-26 18:16:29 +0800

    style: 更新了公告

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,14 +5,14 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "",
 
 	// 公告内容
-	content: "中秋快乐！🥮",
+	content: "欢迎来到我的博客！",
 
 	// 是否允许用户关闭公告
 	closable: true,
 
 	link: {
 		// 启用链接
-		enable: false,
+		enable: true,
 		// 链接文本
 		text: "了解更多",
 		// 链接 URL
